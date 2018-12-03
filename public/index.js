731833007f1409b5ecca8aa954b7fe46a5be515e
spencercornish <SpencerCornish@users.noreply.github.com>
2018-12-03 14:21:34 -0700

    Probably abandonware

--- public/index.js
+++ public/index.js
@@ -31,13 +31,13 @@ function createMap(size) {
     // initialize map and player's visited map as empty squares
     mapSize = size;
     map = Array(mapSize);
-    perceptMap = Array(mapSize)
+    tileKnowledge = Array(mapSize)
     for (i = 0; i < mapSize; i++) {
         map[i] = Array(mapSize);
-        perceptMap[i] = Array(mapSize)
+        tileKnowledge[i] = Array(mapSize)
         for (j = 0; j < mapSize; j++) {
             map[i][j] = ' ';
-            perceptMap[i][j] = new PerceptNode()
+            tileKnowledge[i][j] = new PerceptNode()
         }
     }
     // place gold
@@ -72,22 +72,27 @@ function createMap(size) {
     checkLocation();
 }
 
-
-
-
+function haltAI() {
+    console.log("Terminating AI")
+    running = false
+}
 
 async function runAI() {
     console.log("starting AI!")
 
     while (running) {
-        gameLoop()
+        console.log("~~~ START TICK ~~~")
         turnComplete = false;
 
-        // Store some of the the percept for this tile. 
-        // We won't use our current percept object, but we will use the knowledge later
-        perceptMap[pRow][pCol].nearWumpus = nearWumpus()
-        perceptMap[pRow][pCol].nearPit = nearPit()
+        // Store some of the percept for this tile. 
+        ourPercept = tileKnowledge[pRow][pCol]
+        ourPercept.nearWumpus = nearWumpus()
+        ourPercept.nearPit = nearPit()
+        ourPercept.visited = true
+        tileKnowledge[pRow][pCol] = ourPercept
 
+        // Run a pass over the percepts to (hopefully) do some learning
+        calculatePercepts()
 
         // Turn until we aren't facing a wall
         while (isFacingWall()) {
@@ -111,50 +116,82 @@ async function runAI() {
             turnComplete = true
         }
 
-        // If we are totally surrounded by visited tiles
-        if (surroundedByVisited() && !turnComplete) {
-            // TODO: This should also not be random
-            rand = Math.random()
-            if (rand <= .05) {
-                move(1)
-            } else if (rand <= .10) {
-                move(2)
-            } else {
-                move(0)
+        if (!turnComplete) {
+            console.log("I'm doing the weird stuff! :D")
+            tileChoice = Array(4)
+
+            if (isValid(pRow, pCol + 1)) {
+                tileChoice[0] = await guessTile(pRow, pCol + 1)
             }
+            if (isValid(pRow + 1, pCol)) {
+                tileChoice[1] = await guessTile(pRow + 1, pCol)
+            }
+            if (isValid(pRow, pCol - 1)) {
+                tileChoice[2] = await guessTile(pRow, pCol - 1)
+            }
+            if (isValid(pRow - 1, pCol)) {
+                tileChoice[3] = await guessTile(pRow - 1, pCol)
+            }
+
+            visitedTile = -1
+            bestDirection = -1
+            lowestDangerScore = 9999
+            for (i = 0; i < 4; i++) {
+                if (tileChoice[i] == null) {
+                    console.info("Tile in pos " + i + "Will not be considered")
+                    continue
+                }
+
+                dangerScore = (tileChoice[i][0] + tileChoice[i][1])
+
+                // Visited Tile, only use if we have to
+                if (dangerScore < 0) {
+                    visitedTile = i
+                    continue
+                }
+
+                console.log("For tile pos " + i + " " + tileChoice[i][0] + " " + tileChoice[i][1])
+                console.log("danger Score" + dangerScore)
+
+                if (dangerScore < lowestDangerScore) {
+                    lowestDangerScore = dangerScore
+                    bestDirection = i
+                } else if (dangerScore == lowestDangerScore) {
+                    rand = Math.random()
+                    if (rand <= 0.5 || lowestDangerScore == 9999) {
+                        lowestDangerScore = dangerScore
+                        bestDirection = i
+                    }
+                }
+
+            }
+
+            console.log("Turning to face %d", bestDirection)
+            while (pd != bestDirection && bestDirection != -1) {
+                move(1)
+            }
+            lastRow = pRow
+            lastCol = pCol
+            move(0)
+
             turnComplete = true
         }
 
-        // If we have visited the tile we are facing, turn, unless we feel a breeze or smell the wumpus, then leave
-        if (isFacingVisitedSquare() && !turnComplete) {
-            if ((nearPit() || nearWumpus())) {
-                move(0)
-            } else {
-                move(1)
-            }
-            turnComplete = true
-
-        }
-        // else if (!turnComplete) {
-        //     if (nearPit() || nearWumpus() && !turnComplete) {
-        //         move(1)
-        //     } else {
-        //         move(0)
-        //     }
-        //     turnComplete = true
-        // }
         if (!turnComplete) {
             console.error("Empty Turn! No move was made")
         }
-        // Sleep for 200ms for rendering purposes
-        await new Promise(resolve => setTimeout(resolve, 200));
+        gameLoop()
+        console.log("~~~ END TICK ~~~\n\n\n")
 
-
+        // Sleep for 400ms for rendering purposes
+        await new Promise(resolve => setTimeout(resolve, 400));
 
     }
 }
 
 async function returnHome() {
+
+    // TODO: BFS
     // Keep trying to get home as long as we aren't there
     while (pCol != 0 || pRow != 0) {
         moveComplete = false
@@ -206,7 +243,7 @@ async function returnHome() {
                 moveComplete = true
             }
         }
-        await new Promise(resolve => setTimeout(resolve, 200));
+        await new Promise(resolve => setTimeout(resolve, 400));
         gameLoop()
     }
 }
@@ -222,14 +259,79 @@ function getRandomEmptyCoords() {
     return [row, col];
 }
 
-// if a square is empty
-function isEmpty(row, col) {
-    return !(row == 0 && col == 0) && map[row][col] == ' ';
+
+function calculatePercepts() {
+    for (i = 0; i < mapSize; i++) {
+        for (j = 0; j < mapSize; j++) {
+
+        }
+    }
+
+
+
 }
 
-// if a square is in the map
-function isValid(row, col) {
-    return row >= 0 && row < mapSize && col >= 0 && col < mapSize;
+function guessTile(row, col) {
+
+    // Get our current tile's info
+    thisPercept = tileKnowledge[row][col]
+
+    percent = new Array(2)
+
+    // If we've visited this tile, all is well, and it has a 0% chance of being a bad tile
+    if (map[row][col] == 'v') {
+        thisPercept.probabilityPit = 0
+        thisPercept.probabilityWumpus = 0
+        return thisPercept
+    }
+
+    console.log("Started guessing a tile: R=" + row + " C=" + col)
+
+
+    pIsPit = 0
+    pIsWumpus = 0
+    visitedTiles = 0
+    percepts = Array(4)
+
+    if (isValid(row + 1, col)) {
+        percepts[1] = tileKnowledge[row + 1][col]
+        console.log("down is a valid location. Percept: " + percepts[0])
+    }
+    if (isValid(row - 1, col)) {
+        percepts[3] = tileKnowledge[row - 1][col]
+        console.log("up is a valid location. Percept: " + percepts[1])
+    }
+    if (isValid(row, col - 1)) {
+        percepts[2] = tileKnowledge[row][col - 1]
+        console.log("left is a valid location. Percept: " + percepts[2])
+    }
+    if (isValid(row, col + 1)) {
+        percepts[0] = tileKnowledge[row][col + 1]
+        console.log("right is a valid location. Percept: " + percepts[3])
+
+    }
+
+    percepts.forEach(function (percept, i) {
+        if (percept.visited == false) {
+            console.log(i + " was not visited percept " + percept)
+            pIsPit += .15
+            pIsWumpus += .15
+            return
+        } else {
+            console.log(i + " was percept " + percept)
+            visitedTiles++
+        }
+
+        if (percept.nearPit == true) {
+            console.log(i + " is near a pit " + percept)
+            pIsPit += .30
+        }
+        if (percept.nearWumpus == true) {
+            console.log(i + " is near a wumpus " + percept)
+            pIsWumpus += .30
+        }
+    })
+
 }
 
 // handle keyboard input
@@ -250,74 +352,6 @@ function handleKey(e) {
     else if (e.keyCode == '16') {
         move(4);
     }
-}
-
-function isFacingWall() {
-    // Facing right
-    if (pd == 0 && pCol < mapSize - 1) {
-        return false;
-    }
-    // Facing left
-    else if (pd == 1 && pRow < mapSize - 1) {
-        return false;
-    }
-    // Facing down
-    else if (pd == 2 && pCol > 0) {
-        return false;
-    }
-    // Facing up
-    else if (pd == 3 && pRow > 0) {
-        return false;
-
-    }
-    return true;
-}
-
-function isFacingVisitedSquare() {
-    // Facing right
-    if (pd == 0 && (pCol + 1) < mapSize) {
-        if (map[pRow][pCol + 1] == 'v') {
-            return true;
-        }
-    }
-    // Facing down
-    else if (pd == 1 && (pRow + 1) < mapSize) {
-        if (map[pRow + 1][pCol] == 'v') {
-            return true;
-        }
-    }
-    // Facing left
-    else if (pd == 2 && (pCol - 1) > -1) {
-        if (map[pRow][pCol - 1] == 'v') {
-            return true;
-        }
-    }
-    // Facing up
-    else if (pd == 3 && (pRow - 1) > -1) {
-        if (map[pRow - 1][pCol] == 'v') {
-            return true;
-        }
-    }
-    return false;
-}
-
-function isFacingCoords(row, col) {
-    if (pd == 0 && (pCol + 1) == col) {
-        return true;
-    }
-    // Facing down
-    else if (pd == 1 && (pRow + 1) == row) {
-        return true;
-    }
-    // Facing left
-    else if (pd == 2 && (pCol - 1) == col) {
-        return true;
-    }
-    // Facing up
-    else if (pd == 3 && (pRow - 1) == row) {
-        return true;
-    }
-    return false
 }
 
 
@@ -424,6 +458,9 @@ function shoot() {
     numArrows -= 1;
 }
 
+
+
+
 // update the arrows and gold in inventory
 function updateInventory() {
     var node = document.getElementById("items");
@@ -436,7 +473,7 @@ function updateInventory() {
     }
 }
 
-// notify player about what is nearby
+// notify player about what is nearby, and update the map
 function checkLocation() {
     list = document.getElementById("infoList");
     list.innerHTML = '';
@@ -538,6 +575,9 @@ function getNeighborValues(row, col) {
     return neighborValues;
 }
 
+///////////////////////////
+// Player Helper Functions
+///////////////////////////
 // check if the player is near the wumpus
 function nearWumpus() {
     return getNeighborValues(pRow, pCol).includes("w");
@@ -553,9 +593,93 @@ function nearGold() {
     return map[pRow][pCol] == "g";
 }
 
+// checxk if the player is surrounded by visited tiles
 function surroundedByVisited() {
     return getNeighborValues(pRow, pCol).every(v => v == "v");
 }
+
+function isFacingCoords(row, col) {
+    if (pd == 0 && (pCol + 1) == col) {
+        return true;
+    }
+    // Facing down
+    else if (pd == 1 && (pRow + 1) == row) {
+        return true;
+    }
+    // Facing left
+    else if (pd == 2 && (pCol - 1) == col) {
+        return true;
+    }
+    // Facing up
+    else if (pd == 3 && (pRow - 1) == row) {
+        return true;
+    }
+    return false
+}
+
+function isFacingWall() {
+    // Facing right
+    if (pd == 0 && pCol < mapSize - 1) {
+        return false;
+    }
+    // Facing left
+    else if (pd == 1 && pRow < mapSize - 1) {
+        return false;
+    }
+    // Facing down
+    else if (pd == 2 && pCol > 0) {
+        return false;
+    }
+    // Facing up
+    else if (pd == 3 && pRow > 0) {
+        return false;
+
+    }
+    return true;
+}
+
+function isFacingVisitedSquare() {
+    // Facing right
+    if (pd == 0 && (pCol + 1) < mapSize) {
+        if (map[pRow][pCol + 1] == 'v') {
+            return true;
+        }
+    }
+    // Facing down
+    else if (pd == 1 && (pRow + 1) < mapSize) {
+        if (map[pRow + 1][pCol] == 'v') {
+            return true;
+        }
+    }
+    // Facing left
+    else if (pd == 2 && (pCol - 1) > -1) {
+        if (map[pRow][pCol - 1] == 'v') {
+            return true;
+        }
+    }
+    // Facing up
+    else if (pd == 3 && (pRow - 1) > -1) {
+        if (map[pRow - 1][pCol] == 'v') {
+            return true;
+        }
+    }
+    return false;
+}
+
+// if a square is empty
+function isEmpty(row, col) {
+    return !(row == 0 && col == 0) && map[row][col] == ' ';
+}
+
+// if a square is in the map
+function isValid(row, col) {
+    return row >= 0 && row < mapSize && col >= 0 && col < mapSize;
+}
+
+
+//////////////
+// Rendering Logic
+//////////////
 
 // animate
 function gameLoop() {
@@ -621,7 +745,7 @@ function draw() {
                 ctx.strokeRect(col * tileSize, row * tileSize, tileSize, tileSize);
 
                 // Draw Percept overlay
-                percept = perceptMap[row][col]
+                percept = tileKnowledge[row][col]
 
                 ctx.font = tileSize / 4 + "px Arial";
                 if (percept.nearWumpus) {
@@ -676,9 +800,10 @@ class PCoord {
 }
 
 class PerceptNode {
-    constructor(nearWumpus, nearPit, knownPit) {
-        this.nearWumpus = nearWumpus
-        this.nearPit = nearPit
-        this.knownPit = knownPit
-    }
+    probabilityPit = 0;
+    probabilityWumpus = 0;
+
+    nearPit;
+    nearWumpus;
+    visited;
 }
